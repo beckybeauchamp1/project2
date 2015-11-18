2479c0b7a35895c7b3d923e6cf54dc79e31b243b
Becky Beauchamp <beckybeauchamp1@gmail.com>
2015-11-17 20:08:40 -0500

    Add Photo Create, Edit, and Destroy  Methods in Controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,21 @@ class CommentsController < ApplicationController
   def create
     @retreat = Retreat.find(params[:retreat_id])
     @retreat.comments.create!(comment_params.merge(user: current_user))
+    redirect_to retreat_path(@retreat), notice:
+    "Thanks, #{current_user.firstname} for sharing!"
+  end
+
+  def update
+    @retreat = Retreat.find(params[:retreat_id])
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params.merge(user: current_user))
+    redirect_to retreat_path(@retreat)
+  end
+
+  def destroy
+    @retreat = Retreat.find(params[:retreat_id])
+    @comment= Comment.find(params[:id])
+    @comment.destroy
     redirect_to retreat_path(@retreat)
   end
 
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,4 +3,41 @@ class PhotosController < ApplicationController
     @retreat = Retreat.find(params[:retreat_id])
     @photos = @retreat.photos
   end
+
+  def show
+
+  end
+
+  def new
+    @retreat = Retreat.find(params[:retreat_id])
+    @photo = Photo.new
+  end
+
+  def create
+    @retreat = Retreat.find(params[:retreat_id])
+    @photo = @retreat.photos.create!(photo_params.merge(user: current_user))
+    redirect_to retreat_photos_path(@retreat, @user), notice:
+    "Congrats, #{current_user.firstname} you've just started a new retreat!"
+  end
+
+    def edit
+      @retreat = Retreat.find(params[:retreat_id])
+      @photo = Photo.find(params[:retreat_id])
+    end
+
+    def update
+      @photo = Photo.find(params[:id])
+      @photo.update(photo_params.merge(user: current_user))
+      redirect_to retreat_photos_path(@retreat, @user), notice:
+      "Thanks, #{current_user.firstname} for updating this photo!"
+    end
+
+    def destroy
+    end
+
+    private
+    def photo_params
+      params.require(:photo).permit(:title, :description, :photo_url)
+    end
+
 end
