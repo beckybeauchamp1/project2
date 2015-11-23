0c7fb4f9abdcbaee3006d38a9d5b9c8411f25c6c
Becky Beauchamp <beckybeauchamp1@gmail.com>
2015-11-23 10:04:00 -0500

    Remove form

--- db/schema.rb
+++ db/schema.rb
@@ -53,16 +53,6 @@ ActiveRecord::Schema.define(version: 20151120044649) do
     t.datetime "updated_at",            null: false
   end
 
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "searchable_id"
-    t.string   "searchable_type"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_index "pg_search_documents", ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
-
   create_table "photos", force: :cascade do |t|
     t.string   "title"
     t.string   "photo_url"
